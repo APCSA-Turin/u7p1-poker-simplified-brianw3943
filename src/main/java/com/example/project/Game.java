8b59374d1c57797809e6fd20ec09a692cd9e14bd
brianw3943 <brianw124@nycstudents.net>
2025-03-03 16:59:35 +0000

    commit

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public class Game{
     public static String determineWinner(Player p1, Player p2,String p1Hand, String p2Hand,ArrayList<Card> communityCards){
-        
         return "Error";
     }
 
